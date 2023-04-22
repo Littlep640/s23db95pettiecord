03a8fe3c0d9dac47991ade1a02f935d4b27c7f67
Littlep640 <78877689+Littlep640@users.noreply.github.com>
2023-04-21 23:24:45 -0500

    Part 2 Completed

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require("passport-local").Strategy;
+passport.use(new LocalStrategy)(
+  function(username, password, done) {
+    Account.findOne({username: username}, function(err,user){
+      if(err) {return done(err);}
+      if(!user) {
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      if(!user.validPassword(password)){
+        return done(null, false, {message: 'Incorrect password.'})
+      }
+      return done(null, user);
+    });
+  }
+)
+
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -36,6 +53,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session'))({
+  secret: 'keyboard cat', 
+  resave: false,
+  saveUnitialized: false
+});
+app.use(passport.initialize());
+app.use(passport.session())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -44,6 +69,23 @@ app.use('/dogs', dogsRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter)
+
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser())
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const accountSchema = new Schema({
+  username: String,
+  password: String
+});
+
+accountSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("Account", accountSchema);
 
 
 // catch 404 and forward to error handler
